a45e96649b0174c63c15c1fe89295ee0bedc45ba
Andrei Lupau <93196728+andreilupau@users.noreply.github.com>
2025-03-15 22:06:12 +0200

    Add files via upload

---
+++ Counting numbers/script.js
@@ -0,0 +1,25 @@
+
+// THE TEXT (RESULT)
+let countEl = document.getElementById('count-el')
+count = 0
+
+// Increment by button
+function increment() {
+    count += 1;
+    countEl.innerText = count
+}
+
+// Decrement by button
+function decrement() {
+    count -= 1;
+    countEl.innerText = count
+}
+
+// Previous enteries
+let logsEl = document.getElementById('logs-el')
+
+function save() {
+    logsEl.innerText += ' ' + count;
+    countEl.innerText = 0;
+    count = 0;
+}
